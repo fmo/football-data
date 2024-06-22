a39f9a056939a496e15a64ff1bd66cbab5edffa0
Mustafa Ozyurt <mustafa.ozyurt@gmail.com>
2024-06-22 17:22:52 +0200

    Timeout the request

--- rapidapi/request.go
+++ rapidapi/request.go
@@ -1,25 +1,36 @@
 package rapidapi
 
 import (
+	"context"
 	log "github.com/sirupsen/logrus"
 	"io"
 	"net/http"
 	"os"
+	"time"
 )
 
 func rapidRequest(url string) []byte {
-	req, err := http.NewRequest("GET", url, nil)
+	ctx, cancel := context.WithTimeout(context.Background(), 1*time.Second)
+	defer cancel()
+
+	req, err := http.NewRequestWithContext(ctx, "GET", url, nil)
 	if err != nil {
-		log.Println(err)
+		log.Fatalf("Failed to create request: %v", err)
 	}
 
 	req.Header.Add("X-RapidAPI-Key", os.Getenv("RAPID_API_KEY"))
 	req.Header.Add("X-RapidAPI-Host", "api-football-v1.p.rapidapi.com")
 
-	res, _ := http.DefaultClient.Do(req)
-
+	res, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatalf("Request failed: %v", err)
+	}
 	defer res.Body.Close()
-	body, _ := io.ReadAll(res.Body)
+
+	body, err := io.ReadAll(res.Body)
+	if err != nil {
+		log.Fatalf("Failed to read response body: %v", err)
+	}
 
 	return body
 }
